781a5ae558b3b27d390a01b650f1cafe9c1c7073
truong288 <truongtuong.k20@gmail.com>
2025-05-05 02:57:07 +0700

    main.py

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     threading.Thread(target=run_flask).start()
-    TOKEN = "7243590811:AAGY-Py_DP_561bc2DsPjFKkZTuvp******"
+    TOKEN = "7243590811:AAGY-Py_DP_561bc2DsPjFKkZTuvp7mSl0o"
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("startgame", start_game))
